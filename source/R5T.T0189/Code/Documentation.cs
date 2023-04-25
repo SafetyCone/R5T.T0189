d18a19818ab16838f90942b4ce07e7f1a9bf3f90
davidcoats <david@getsafetycone.com>
2023-04-25 14:42:13 -0700

    Initial commit.

---
+++ source/R5T.T0189/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0189
+{
+	/// <summary>
+	/// Run-specific, date-specific, and application-specific directory path strong-types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0189/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0189
+Run-specific, date-specific, and application-specific directory path strong-types.
